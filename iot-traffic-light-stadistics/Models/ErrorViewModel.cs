16f639d2e0ff49f882402b1df3cbbe8f590e5bcb
Enmanuel Balcacer <makuttico@gmail.com>
2021-04-27 04:28:11 -0400

    Agregando todo al repo

---
+++ iot-traffic-light-stadistics/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace iot_traffic_light_stadistics.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
